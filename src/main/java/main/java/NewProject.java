5b4cbb86c372b0febe79544b9fb899c1de42c82e
umalatgordanov <gordanov98@inbox.ru>
2021-08-08 10:32:27 +0300

    change class newproject1

--- src/main/java/main/java/NewProject.java
+++ src/main/java/main/java/NewProject.java
@@ -3,8 +3,7 @@ package main.java;
 import java.util.stream.Stream;
 
 public class NewProject {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Stream.builder().add("helloWorld").build().forEach(System.out::println);
     }
 }
