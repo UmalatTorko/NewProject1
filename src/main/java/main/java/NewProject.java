77070ed5eab3d8fe801df54dc6db9adb9505d7a9
umalatgordanov <gordanov98@inbox.ru>
2021-08-08 10:14:08 +0300

    init commit

---
+++ src/main/java/main/java/NewProject.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import java.util.stream.Stream;
+
+public class NewProject {
+    public static void main(String[] args)
+    {
+        Stream.builder().add("helloWorld").build().forEach(System.out::println);
+    }
+}
